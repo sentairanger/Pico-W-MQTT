5d362e080f189d32580ce9e1e88c10894f8c0c92
sentairanger <ed2point0@gmail.com>
2023-12-04 13:44:17 -0700

    Create mqtt_dual_led.py

---
+++ AdafruitIO/mqtt_dual_led.py
@@ -0,0 +1,70 @@
+from time import sleep
+import network
+from picozero import LED
+from umqtt.simple import MQTTClient
+
+red = LED(0)
+blue = LED(1)
+
+# Fill in your WiFi network name (ssid) and password here:
+wifi_ssid = ""
+wifi_password = ""
+
+# Connect to WiFi
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(wifi_ssid, wifi_password)
+while wlan.isconnected() == False:
+    print('Waiting for connection...')
+    sleep(1)
+print("Connected to WiFi")
+
+# Fill in your Adafruit IO Authentication and Feed MQTT Topic details
+mqtt_host = "io.adafruit.com"
+mqtt_username = ""  # Your Adafruit IO username
+mqtt_password = ""  # Adafruit IO Key
+mqtt_receive_topic = ""  # The MQTT topic for your Adafruit IO Feed
+
+# Enter a random ID for this MQTT Client
+# It needs to be globally unique across all of Adafruit IO.
+mqtt_client_id = "somethingreallyrandomandunique123"
+
+# Initialize our MQTTClient and connect to the MQTT server
+mqtt_client = MQTTClient(
+        client_id=mqtt_client_id,
+        server=mqtt_host,
+        user=mqtt_username,
+        password=mqtt_password)
+
+# So that we can respond to messages on an MQTT topic, we need a callback
+# function that will handle the messages.
+def mqtt_subscription_callback(topic, message):
+    print (f'Topic {topic} received message {message}')  # Debug print out of what was received over MQTT
+    if message == b'blue':
+        print("blue")
+        red.off()
+        blue.on()
+    elif message == b'red':
+        print("red")
+        red.on()
+        blue.off()
+
+# Before connecting, tell the MQTT client to use the callback
+mqtt_client.set_callback(mqtt_subscription_callback)
+mqtt_client.connect()
+
+# Once connected, subscribe to the MQTT topic
+mqtt_client.subscribe(mqtt_receive_topic)
+print("Connected and subscribed")
+
+try:
+    while True:
+        # Infinitely wait for messages on the topic.
+        # Note wait_msg() is a blocking call, if you're doing multiple things
+        # on the Pico you may want to look at putting this on another thread.
+        print(f'Waiting for messages on {mqtt_receive_topic}')
+        mqtt_client.wait_msg()
+except Exception as e:
+    print(f'Failed to wait for MQTT messages: {e}')
+finally:
+    mqtt_client.disconnect()
